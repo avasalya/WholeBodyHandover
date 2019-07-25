544138c1d5634c3d3c5f813a761d8eec552503fa
ashesh vasalya <ashesh@gite.lirmm.fr>
2019-07-25 19:47:33 +0900

    minor change

--- src/handover_controller.cpp
+++ src/handover_controller.cpp
@@ -74,8 +74,7 @@ namespace mc_handover
 		// qpsolver->addConstraintSet(kinematicsConstraint);
 
 
-		LOG_SUCCESS("mc_handover_controller init done")
-
+		LOG_SUCCESS("Stabilized_handoverController init done")
 	}
 
 
@@ -367,4 +366,4 @@ namespace mc_handover
 
 } //namespace mc_control
 
-CONTROLLER_CONSTRUCTOR("Handover2", mc_handover::HandoverController)
+CONTROLLER_CONSTRUCTOR("Handover", mc_handover::HandoverController)
